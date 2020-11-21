d43f33cee5e76ce2a42a765c1f40cb758987dd00
Samy Abdouche <samy.abdouche@gmail.com>
2020-11-21 23:24:40 +0100

    Changement important matrice adjacence

--- src/graph.c
+++ src/graph.c
@@ -34,17 +34,17 @@ int **adjacency_matrix_creation(ListOfCities *cities){
 	int i, j;
 	//Allocation of each row. However, using the diagonal properties
 	//of an adjacency matrix, we do not need to create an n*n matrix.
-	//We only need n*(n-1)/2 cells to store our valuations.
+	//We only need n*(n+1)/2 cells to store our valuations.
 	for (i = 0; i < cities->number; i++)
-		if ((matrix[i] = (int *)malloc(cities->number * sizeof(int))) == NULL){
+		if ((matrix[i] = (int *)malloc((cities->number-i)* sizeof(int))) == NULL){
 			perror("Memory allocation failed. Exit. \n");
 			exit(EXIT_FAILURE);
 		}
 	
 	//Now, we'll fill our matrix.
 	for (i = 0; i < cities->number; i++)
-		for (j = 0; j < cities->number; j++)
-			matrix[i][j] = edge_valuation(cities, i, j) ;
+		for (j = 0; j < cities->number - i; j++)
+			matrix[i][j] = edge_valuation(cities, i, j+i) ;
 
 	return (matrix);
 }
@@ -61,7 +61,7 @@ void display_matrix (ListOfCities* cities, int** matrix)
 		printf("\n");
 		printf("%.5s \t", cities->name[i]);
 
-		for (int j = 0; j < cities->number; j++)
+		for (int j = 0; j < cities->number-i; j++)
 			printf("%i \t", matrix[i][j]);
 	}
 	printf("\n\n") ;
@@ -74,9 +74,9 @@ void saveGraph_alt(int **matrix, int dimension){
 	fileOut = fopen("resuGraph.dat", "w");
 
 	for (int i = 0; i < dimension; i++)
-		for (int j = 0; j < i; j++)
+		for (int j = 0; j < dimension - i; j++)
 			if (matrix[i][j] != 0)
-				fprintf(fileOut, "%i %i\n", i, j);
+				fprintf(fileOut, "%i %i\n", i, j+i);
 
 	fclose(fileOut);
 
