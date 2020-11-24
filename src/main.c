f0446ddd29649aee14c82a58701f79050cca45e6
Samy Abdouche <samy.abdouche@gmail.com>
2020-11-24 17:04:37 +0100

    main

--- src/main.c
+++ src/main.c
@@ -1,12 +1,34 @@
 #include "citiesReader.h"
 #include "graph.h"
 #include "heap.h"
+#include "analyzer.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
+
+
+
+struct timespec diff(struct timespec start, struct timespec end)
+{
+    struct timespec temp;
+
+    if ((end.tv_nsec-start.tv_nsec)<0)
+    {
+            temp.tv_sec = end.tv_sec-start.tv_sec-1;
+            temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+    }
+    else 
+    {
+            temp.tv_sec = end.tv_sec-start.tv_sec;
+            temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+    }
+    return temp;
+}
+
+
 
 int main(int argc, char ** argv)
 {
@@ -17,32 +39,33 @@ int main(int argc, char ** argv)
 		perror("Veuillez passer une population minimale en paramètre du programme\n");
 		return EXIT_FAILURE;
 	}
-	
-	//-----------------------------------------------------------------
-	//--- READING cities
-	//-----------------------------------------------------------------
-
-	int popMin = atoi(argv[1]);
 
 
-
-	ListOfCities* cities;
-	cities = citiesReader(popMin);
-
-	//-----------------------------------------------------------------
-	//--- COMPUTING complete graph
-	//-----------------------------------------------------------------
-
+	int popMin = atoi(argv[1]), total_cost = 0;
+	ListOfCities* cities = citiesReader(popMin);
 	int **matrix = adjacency_matrix_creation(cities->number);
-	int total_cost = 0;
+	analyzer_t *time_analysis = analyzer_create();
+	struct timespec before, after, difference;
+	clockid_t clk_id = CLOCK_REALTIME;
+
+
+
 	adjacency_matrix_filling(matrix, cities);
 
-	// Displaying the matrix
-	display_matrix (cities, matrix);
-	//Processing an AST for our complete graph.
-	matrix = prim(matrix, cities->number, &total_cost);
+	for (int i = 0; i < 100; i++){
+		//display_matrix (cities, matrix);
+		clock_gettime(clk_id, &before);
+		matrix = prim(matrix, cities->number);
+		clock_gettime(clk_id, &after);
+		save_values(time_analysis, "test1.txt");
+		difference = diff(before, after);
+		analyzer_append(time_analysis, difference.tv_nsec);
 
-	printf("The length of the optimal network is of %d\n", total_cost);
+		//DIsplaying a few statistics on the experience.
+	}
+
+	printf("%Lf\n", get_average_cost(time_analysis));
+	//printf("The length of the optimal network is of %d\n", total_cost);
 
 	//Saving the graph in a text file
 
